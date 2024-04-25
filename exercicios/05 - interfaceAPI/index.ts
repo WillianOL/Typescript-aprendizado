5f33e6e8051caddb651681d0a34bfab6bcb6a436
Willian Oliveira <williancontato67@gmail.com>
2024-04-25 20:05:18 -0300

    Finalização do execicio de definição de interface dos dados de uma API

---
+++ exercicios/05 - interfaceAPI/index.ts
@@ -0,0 +1,42 @@
+// Defina a interface da API: https://api.origamid.dev/json/cursos.json e mostre os dados na tela.
+// Existem apenas dois níveis de cursos, Iniciante (iniciante) e Avançado (avancado). Se for para iniciante pinte o título de azul, para avançado pinte de vermelho.
+
+interface Curso {
+  nome: string;
+  horas: number;
+  aulas: number;
+  gratuito: boolean;
+  tags: string[];
+  idAulas: number[];
+  nivel: 'iniciante' | 'avancado';
+}
+
+async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const data = await response.json();
+  mostrarCursos(data);
+}
+
+fetchCursos();
+
+function mostrarCursos(cursos: Array<Curso>) {
+  cursos.forEach(({ aulas, nivel, nome, horas, gratuito, tags, idAulas }) => {
+    let cor;
+    if (nivel === 'iniciante') {
+      cor = '#07F';
+    } else if (nivel === 'avancado') {
+      cor = '#F55';
+    }
+    document.body.innerHTML += `
+      <div>
+        <h2 style="color: ${cor}">${nome}</h2>
+        <p style="color: ${cor}">Nivel: ${nivel}</p>
+        <p>Horas: ${horas}hrs</p>
+        <p>Número de aulas: ${aulas}</p>
+        <p>Tags: ${tags.join(", ")}</p>
+        <p>Aulas: ${idAulas.join(", ")}</p>
+        <p>Gratuito: ${gratuito ? 'Sim' : 'Não'}</p>
+      </div>
+    `;
+  });
+}
