a2047b9b1360c0076be9898f6b2de11a68ca3e6f
Willian Oliveira <williancontato67@gmail.com>
2024-05-20 16:36:08 -0300

    Finalização do exercicio sobre function overload

---
+++ exercicios/09 - functionArredondamento/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// Crie uma função que arredonda um valor passado para cima.
+// A função pode receber string ou number.
+// A função deve retornar o mesmo tipo que ela receber.
+function arredondar(valor) {
+    if (typeof valor === 'number') {
+        return Math.ceil(valor);
+    }
+    else {
+        return Math.ceil(Number(valor)).toString();
+    }
+}
+console.log(arredondar(200.5));
+console.log(arredondar("10.2"));
